c22761dd65ed9342baf69700554547120ce6760c
Kyle <keaton12@Live.com>
2015-03-20 11:35:27 -0500

    First Files

---
+++ MyGLRenderer.java
@@ -0,0 +1,147 @@
+package com.example.kyle.shaderexample;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLES31;
+import java.nio.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Kyle on 3/12/2015.
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer{
+
+    int program;
+    private int PositionHandle;
+
+    private FloatBuffer vertexBuffer;
+    private int mProgram;
+    private int mPositionHandle;
+    private int mColorHandle;
+    private int mMVPMatrixHandle;
+
+    // number of coordinates per vertex in this array
+    static final int COORDS_PER_VERTEX = 3;
+    static float triangleCoords[] = {
+            // in counterclockwise order:
+            0.0f,  0.622008459f, 0.0f,   // top
+            -0.5f, -0.311004243f, 0.0f,   // bottom left
+            0.5f, -0.311004243f, 0.0f    // bottom right
+    };
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 0.0f };
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        // initialize vertex byte buffer for shape coordinates
+        ByteBuffer bb = ByteBuffer.allocateDirect(
+                // (number of coordinate values * 4 bytes per float)
+                triangleCoords.length * 4);
+        // use the device hardware's native byte order
+        bb.order(ByteOrder.nativeOrder());
+
+        // create a floating point buffer from the ByteBuffer
+        vertexBuffer = bb.asFloatBuffer();
+        // add the coordinates to the FloatBuffer
+        vertexBuffer.put(triangleCoords);
+        // set the buffer to read the first coordinate
+        vertexBuffer.position(0);
+
+
+        program = GLES31.glCreateProgram();
+        int vertexShader = GLES31.glCreateShader(GLES31.GL_VERTEX_SHADER);
+        int fragmentShader = GLES31.glCreateShader(GLES31.GL_FRAGMENT_SHADER);
+
+        StringBuilder vertexSource = new StringBuilder();
+        StringBuilder fragmentSource = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("normalmap_vs.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                vertexSource.append(line).append('\n');
+            }
+            reader.close();
+
+        } catch (IOException e) {}
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("normalmap_fs.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                fragmentSource.append(line).append('\n');
+            }
+            reader.close();
+
+        } catch (IOException e) {}
+
+        System.out.println(fragmentSource);
+
+        GLES31.glShaderSource(vertexShader, vertexSource.toString());
+        GLES31.glCompileShader(vertexShader);
+        GLES31.glShaderSource(fragmentShader, fragmentSource.toString());
+        GLES31.glCompileShader(fragmentShader);
+
+        GLES31.glAttachShader(program, vertexShader);
+        GLES31.glAttachShader(program, fragmentShader);
+
+        GLES31.glLinkProgram(program);
+        GLES31.glValidateProgram(program);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+        GLES31.glUseProgram(program);
+
+        // get handle to vertex shader's vPosition member
+        mPositionHandle = GLES31.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES31.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES31.glVertexAttribPointer(
+                mPositionHandle, COORDS_PER_VERTEX,
+                GLES31.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES31.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the triangle
+        GLES31.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES31.glGetUniformLocation(mProgram, "uMVPMatrix");
+        //MyGLRenderer.checkGlError("glGetUniformLocation");
+
+        // Apply the projection and view transformation
+        //GLES31.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+        //MyGLRenderer.checkGlError("glUniformMatrix4fv");
+
+        // Draw the triangle
+        GLES31.glDrawArrays(GLES31.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES31.glDisableVertexAttribArray(mPositionHandle);
+
+
+    }
+}
---
+++ ShaderExample.java
@@ -0,0 +1,150 @@
+package com.example.kyle.shaderexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.opengl.GLES31;
+import android.opengl.GLES31Ext;
+import java.lang.String;
+
+import android.util.Log;
+import java.io.*;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLSurfaceView.Renderer;
+
+public class ShaderExample extends Activity {
+
+    private static final String TAG = "ShaderExample";
+    StringBuffer vs;
+    StringBuffer fs;
+    private GLSurfaceView mainView;
+    Renderer renderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //readShader();
+        //int program = createProgram(vs.toString(), fs.toString());
+
+        //=====
+
+        mainView = new GLSurfaceView(this);
+        setContentView(mainView);
+
+        //=====
+
+        // Create a new GLSurfaceView - this holds the GL Renderer
+        /*mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        renderer = new Renderer(this);
+        mGLSurfaceView.setRenderer(renderer);
+
+
+        renderer.setShader(this.NORMALMAP_SHADER);*/
+
+        setContentView(R.layout.activity_shader_example);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_shader_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*public int createProgram(String vertexSource, String fragmentSource) {
+        int vertexShader = loadShader(GLES31.GL_VERTEX_SHADER, vertexSource);
+        int pixelShader = loadShader(GLES31.GL_FRAGMENT_SHADER, fragmentSource);
+
+        int program = GLES31.glCreateProgram();
+        if (program != 0) {
+            GLES31.glAttachShader(program, vertexShader);
+            //checkGlError("glAttachShader");
+            GLES31.glAttachShader(program, pixelShader);
+            //checkGlError("glAttachShader");
+            GLES31.glLinkProgram(program);
+            int[] linkStatus = new int[1];
+            GLES31.glGetProgramiv(program, GLES31.GL_LINK_STATUS, linkStatus, 0);
+            if (linkStatus[0] != GLES31.GL_TRUE) {
+                Log.e(TAG, "Could not link program: ");
+                Log.e(TAG, GLES31.glGetProgramInfoLog(program));
+                GLES31.glDeleteProgram(program);
+                program = 0;
+            }
+        }
+        return program;
+    }
+
+    private int loadShader(int shaderType, String source) {
+        int shader = GLES31.glCreateShader(shaderType);
+        if (shader != 0) {
+            GLES31.glShaderSource(shader, source);
+            GLES31.glCompileShader(shader);
+            int[] compiled = new int[1];
+            GLES31.glGetShaderiv(shader, GLES31.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {
+                Log.e(TAG, "Could not compile shader " + shaderType + ":");
+                Log.e(TAG, GLES31.glGetShaderInfoLog(shader));
+                GLES31.glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+        return shader;
+    }
+
+    private void readShader() {
+
+        vs = new StringBuffer();
+        fs = new StringBuffer();
+
+        // read the files
+        try {
+
+            InputStream inputStream = getResources().openRawResource(R.raw.normalmap_vs);
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
+            String read = in.readLine();
+            while (read != null) {
+                vs.append(read + "\n");
+                read = in.readLine();
+            }
+            vs.deleteCharAt(vs.length() - 1);
+
+
+
+            inputStream = getResources().openRawResource(R.raw.normalmap_ps);
+            in = new BufferedReader(new InputStreamReader(inputStream));
+
+            read = in.readLine();
+            while (read != null) {
+                fs.append(read + "\n");
+                read = in.readLine();
+            }
+            fs.deleteCharAt(fs.length() - 1);
+
+        } catch (Exception e) {
+            Log.d("ERROR-readingShader", "Could not read shader: " + e.getLocalizedMessage());
+        }
+
+        return;
+    }*/
+
+}
